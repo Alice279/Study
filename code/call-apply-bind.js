250810303c2e93d5c1d0043ac51b1cfe1b342f0d
Alice279 <tianrun279@163.com>
2021-10-15 19:51:39 +0800

    (feat): apply call bind and new

--- code/call-apply-bind.js
+++ code/call-apply-bind.js
@@ -26,30 +26,67 @@ Function.prototype.myBind = function(context, ...args) {
     }
 }
 
-Function.prototype.myBind = function(context, ...args) {
-    const _this = this
-    return function Bind (...newArgs) {
-        if (this instanceof Bind) {
-            return _this.myApply(this, [...args, ...newArgs])
-        }
-        else {
-            return _this.myApply(context, [...args, ...newArgs])
-        }
-    }
-}
+//===================================================================
 
-Function.prototype.myCall = function(context = window, ...args) {
+Function.prototype.call2 = function (context) {
     context = context || window
     context.fn = this
-    const result = context.fn(...args)
+
+    var args = []
+    for (var i = 1, len = arguments.length; i < len; i++) {
+        args.push('arguments['+ i +']')
+    }
+
+    var result = eval('context.fn('+ args +')')
+
     delete context.fn
     return result
 }
 
-Function.prototype.myApply = function(context = window, args = []) {
+Function.prototype.apply2 = function (context, arr) {
     context = context || window
-    ontext.fn = this
-    const result = context.fn(...args)
+    context.fn = this
+
+    var result 
+    if (!arr) {
+        result = context.fn()
+    }
+    else {
+        var args = []
+        for (var i = 0, len = arr.length; i < len; i++) {
+            args.push('arr['+ i +']')
+        }
+        result = eval('context.fn('+ args +')')
+    }
+
     delete context.fn
     return result
+}
+
+//call的实现中，参数通过arguments传递，省略掉位置是0的context
+//apply中，arr是参数，直接从0开始
+
+//当传入的参数是null或者undefined时会自动指向全局对象
+//当传入的参数是原始值(数字，字符串，布尔值)时this指向该原始值的自动包装对象
+
+Function.prototype.bind2 = function (context) {
+    if (typeof this !== 'function') {
+        throw new Error('调用bind的不是函数！')
+    }
+
+    var self = this
+    var args = Array.prototype.slice.call(arguments,1)
+
+    var fNOP = function () {}
+
+    var fBound = function () {
+        var bindArgs = Array.prototype.slice.call(arguments)
+        return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs))
+        //当bind返回的函数作为构造函数时，bind时指定的this失效，但是传入的参数依然有效
+        //这时this 指向构造函数生成的实例呀
+    }
+
+    fNOP.prototype = this.prototype
+    fBound.prototype = new fNOP()
+    return fBound
 }
--- code/new.js
+++ code/new.js
@@ -2,7 +2,7 @@
 
 function _new(fnc, ...args) {
     const obj = Object.create(fnc.prototype)
-    const ret = fnc.apply(obj, arg)
+    const ret = fnc.apply(obj, args)
     return ret instanceof Object ? ret : obj
 }
 
@@ -17,4 +17,17 @@ function mynew(fnc, ...args){
     Object.setPrototypeOf(obj, fnc.prototype)
     const result = fnc.apply(obj, args)
     return result instanceof Object ? result : obj
+}
+
+function my_new () {
+    var obj = new Object()
+    Constructor = [].shift.call(arguments)   
+    //取出第一个参数，就是我们要传入的构造函数
+    obj.__propto__ = Constructor.prototype
+    //将obj的原型指向构造函数，这样实例就可以访问到构造函数原型中的属性
+    var ret = Constructor.apply(obj, arguments)
+    //使用apply改变构造函数的this指向到新生成的实例，这样实例就可以访问到构造函数中的属性
+    return typeof ret === 'object' ? ret : obj
+    //判断返回值是不是一个对象，是则返回对象，不是则该返回啥返回啥
+    //有点构造函数返回对象，有的就不是
 }
